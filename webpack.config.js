e9291a3b17c55f818d2f49d2d19e509328edfb35
Nagendra <nagendra.anasuri08@gmail.com>
2025-12-08 20:36:12 +0530

    Use Netlify remotes in production

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,23 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 
+const isProd = process.env.NODE_ENV === "production";
+
+const PRODUCTS_REMOTE_URL = isProd
+  ? "https://microfe-products.netlify.app/remoteEntry.js"
+  : "http://localhost:3001/remoteEntry.js";
+
+const CART_REMOTE_URL = isProd
+  ? "https://microfe-cart.netlify.app/remoteEntry.js"
+  : "http://localhost:3002/remoteEntry.js";
+
+const ACCOUNT_REMOTE_URL = isProd
+  ? "https://microfe-account.netlify.app/remoteEntry.js"
+  : "http://localhost:3003/remoteEntry.js";
+
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  mode: isProd ? "production" : "development",
   output: {
     publicPath: "auto",
     path: path.resolve(__dirname, "dist"),
@@ -28,21 +42,17 @@ module.exports = {
     extensions: [".js", ".jsx"],
   },
   plugins: [
-    new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        products: "products@http://localhost:3001/remoteEntry.js",
-        cart: "cart@http://localhost:3002/remoteEntry.js",
-        account: "account@http://localhost:3003/remoteEntry.js",
-      },
-    //   shared: {
-    //     react: { singleton: true, requiredVersion: false },
-    //     "react-dom": { singleton: true, requiredVersion: false },
-    //     "react-router-dom": { singleton: true, requiredVersion: false },
-    //   },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+  new ModuleFederationPlugin({
+    name: "container",
+    remotes: {
+      products: `products@${PRODUCTS_REMOTE_URL}`,
+      cart: `cart@${CART_REMOTE_URL}`,
+      account: `account@${ACCOUNT_REMOTE_URL}`,
+    },
+  }),
+  new HtmlWebpackPlugin({
+    template: "./public/index.html",
+  }),
+],
+
 };
